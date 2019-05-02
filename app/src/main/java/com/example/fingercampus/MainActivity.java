e7e6f88368243c7ed821d4529a141f73d4626520
AME1314 <35635358+AME1314@users.noreply.github.com>
2019-05-02 22:38:36 +0800

    1.注册按钮改为SVG矢量图形（drawable/ic_zhuce.xml) 2.测试阿里图标库（未成功）在StudentLoginActivity.java中注释一部分代码勿动 3.新建assets文件夹存放矢量图标 4.String中加入图标Unicode代码 5.删除Activity_main.xml中hello world

--- app/src/main/java/com/example/fingercampus/MainActivity.java
+++ app/src/main/java/com/example/fingercampus/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.fingercampus;
 
 import android.content.SharedPreferences;
+import android.graphics.Typeface;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
--- app/src/main/java/com/example/fingercampus/StudentLoginActivity.java
+++ app/src/main/java/com/example/fingercampus/StudentLoginActivity.java
@@ -4,11 +4,13 @@ import android.annotation.SuppressLint;
 import android.app.Activity;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.graphics.Typeface;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.android.volley.Request;
@@ -50,6 +52,9 @@ public class StudentLoginActivity extends Activity {
             startActivity(new Intent(StudentLoginActivity.this, MainActivity.class));
         }
         setContentView(R.layout.activity_start);
+//        Typeface typeface=Typeface.createFromAsset(getAssets(),"iconfont/iconfont.ttf");
+//        TextView textView=findViewById(R.id.test);
+//        textView.setTypeface(typeface);
         init();
     }
 
