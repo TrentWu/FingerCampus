573fbd53053894fbda9dc5f882f287925805c2a3
AME1314 <35635358+AME1314@users.noreply.github.com>
2019-05-08 14:41:19 +0800

    1.报修界面（仅编写了布局以及基本样式，UI未加，activity未加） 2.在主页面增加“报修”入口

--- app/src/main/java/com/example/fingercampus/MainActivity.java
+++ app/src/main/java/com/example/fingercampus/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.fingercampus;
 
+import android.content.Intent;
 import android.content.SharedPreferences;
 import android.graphics.Typeface;
 import android.support.v7.app.AppCompatActivity;
