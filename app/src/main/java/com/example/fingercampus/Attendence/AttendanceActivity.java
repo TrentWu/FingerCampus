040127f1716ddd1f89d9aa761084b22b881a3c09
传成 吴 <429223306@qq.com>
2019-05-16 21:13:00 +0800

    开始编写考勤类

---
+++ app/src/main/java/com/example/fingercampus/Attendence/AttendanceActivity.java
@@ -0,0 +1,18 @@
+package com.example.fingercampus.Attendence;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+
+public class AttendanceActivity extends Activity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        init();
+    }
+
+    private void init(){
+        
+    }
+}
