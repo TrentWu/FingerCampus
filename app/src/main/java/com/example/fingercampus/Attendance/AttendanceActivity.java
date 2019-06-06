a83e5e6c734a9990e5e5fb95a4acc5b27fd83c5f
传成 吴 <429223306@qq.com>
2019-06-06 12:56:51 +0800

    修改toolbar菜单样式及内容

--- app/src/main/java/com/example/fingercampus/Attendance/AttendanceActivity.java
+++ app/src/main/java/com/example/fingercampus/Attendance/AttendanceActivity.java
@@ -4,6 +4,9 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 
+/**
+ * 考勤活动类
+ */
 public class AttendanceActivity extends Activity {
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
