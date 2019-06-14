516b071023242712e3dfc12c4f706efb8dcd1e77
XuFuying <1430979779@qq.com>
2019-06-14 18:13:34 +0800

    222

--- app/src/main/java/com/example/fingercampus/Timetable/TimetableActivity.java
+++ app/src/main/java/com/example/fingercampus/Timetable/TimetableActivity.java
@@ -1,23 +1,107 @@
 package com.example.fingercampus.Timetable;
 
 import android.app.Activity;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.graphics.Typeface;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+import android.widget.Toolbar;
 
 import com.example.fingercampus.R;
+
+import java.util.ArrayList;
 
 public class TimetableActivity extends Activity {
 
     Typeface typeface;
 
+    //星期几
+    private RelativeLayout day;
+
+    //SQLite Helper类
+    private DatabaseHelper databaseHelper = new DatabaseHelper(this, "database.db", null, 1);
+
+    int currentCoursesNumber = 0;
+    int maxCoursesNumber = 0;
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.timetable);
+
+        //工具条
+        Toolbar toolbar=findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        //从数据库读取数据
+        loadData();
+
         init();
+    }
+
+    //从数据库加载数据
+    private void loadData(){
+        ArrayList<Course> coursesList = new ArrayList<>();//课程列表
+        SQLiteDatabase sqLiteDatabase = databaseHelper.getWritableDatabase();
+        Cursor cursor=sqLiteDatabase.rawQuery("select * from courses",null);
+        if(cursor.moveToFirst()){
+            do {
+                coursesList.add(new Course(
+                        cursor.getString(cursor.getColumnIndex("course_name")),
+                        cursor.getString(cursor.getColumnIndex("teacher")),
+                        cursor.getString(cursor.getColumnIndex("class_room")),
+                        cursor.getInt(cursor.getColumnIndex("day")),
+                        cursor.getInt(cursor.getColumnIndex("class_start")),
+                        cursor.getInt(cursor.getColumnIndex("class_end"))));
+            }while (cursor.moveToNext());
+        }
+        cursor.close();
+
+        //使用从数据库读取出来的课程信息来加载课程表视图
+        for (Course course : coursesList) {
+            createLeftView(course);
+            createItemCourseView(course);
+        }
+    }
+
+    //保存数据到数据库
+    private void saveData(Course course) {
+        SQLiteDatabase sqLiteDatabase =  databaseHelper.getWritableDatabase();
+        sqLiteDatabase.execSQL
+                ("insert into courses(course_name, teacher, class_room, day, class_start, class_end) " + "values(?, ?, ?, ?, ?, ?)",
+                        new String[] {course.getCourseName(),
+                                course.getTeacher(),
+                                course.getClassRoom(),
+                                course.getDay()+"",
+                                course.getStart()+"",
+                                course.getEnd()+""}
+                );
+    }
+
+    //创建"第几节数"视图
+    private void createLeftView(Course course) {
+        int endNumber = course.getEnd();
+        if (endNumber > maxCoursesNumber) {
+            for (int i = 0; i < endNumber-maxCoursesNumber; i++) {
+                View view = LayoutInflater.from(this).inflate(R.layout.left_view, null);
+                LinearLayout.LayoutParams params = new LinearLayout.LayoutParams(110,180);
+                view.setLayoutParams(params);
+
+                TextView text = view.findViewById(R.id.class_number_text);
+                text.setText(String.valueOf(++currentCoursesNumber));
+
+                LinearLayout leftViewLayout = findViewById(R.id.left_view_layout);
+                leftViewLayout.addView(view);
+            }
+            maxCoursesNumber = endNumber;
+        }
     }
     private void init() {
         typeface = Typeface.createFromAsset(getAssets(),"iconfont/iconfont.ttf");
