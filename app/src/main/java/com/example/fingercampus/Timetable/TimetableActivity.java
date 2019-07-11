9b4ed0691237073e269504a669b36f9c0d9c5cd7
XuFuying <1430979779@qq.com>
2019-07-11 16:49:46 +0800

    添加课程界面

--- app/src/main/java/com/example/fingercampus/Timetable/TimetableActivity.java
+++ app/src/main/java/com/example/fingercampus/Timetable/TimetableActivity.java
@@ -1,6 +1,7 @@
 package com.example.fingercampus.Timetable;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.graphics.Typeface;
@@ -37,86 +38,98 @@ public class TimetableActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.timetable);
 
-        //工具条
-        Toolbar toolbar=findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
-
-
-        //从数据库读取数据
-        loadData();
-
+//        //工具条
+//        Toolbar toolbar=findViewById(R.id.toolbar);
+//        setSupportActionBar(toolbar);
+//
+//
+//        //从数据库读取数据
+//        loadData();
+//
         init();
     }
 
-    //从数据库加载数据
-    private void loadData(){
-        ArrayList<Course> coursesList = new ArrayList<>();//课程列表
-        SQLiteDatabase sqLiteDatabase = databaseHelper.getWritableDatabase();
-        Cursor cursor=sqLiteDatabase.rawQuery("select * from courses",null);
-        if(cursor.moveToFirst()){
-            do {
-                coursesList.add(new Course(
-                        cursor.getString(cursor.getColumnIndex("course_name")),
-                        cursor.getString(cursor.getColumnIndex("teacher")),
-                        cursor.getString(cursor.getColumnIndex("class_room")),
-                        cursor.getInt(cursor.getColumnIndex("day")),
-                        cursor.getInt(cursor.getColumnIndex("class_start")),
-                        cursor.getInt(cursor.getColumnIndex("class_end"))));
-            }while (cursor.moveToNext());
-        }
-        cursor.close();
-
-        //使用从数据库读取出来的课程信息来加载课程表视图
-        for (Course course : coursesList) {
-            createLeftView(course);
-            createItemCourseView(course);
-        }
-    }
-
-    //保存数据到数据库
-    private void saveData(Course course) {
-        SQLiteDatabase sqLiteDatabase =  databaseHelper.getWritableDatabase();
-        sqLiteDatabase.execSQL
-                ("insert into courses(course_name, teacher, class_room, day, class_start, class_end) " + "values(?, ?, ?, ?, ?, ?)",
-                        new String[] {course.getCourseName(),
-                                course.getTeacher(),
-                                course.getClassRoom(),
-                                course.getDay()+"",
-                                course.getStart()+"",
-                                course.getEnd()+""}
-                );
-    }
-
-    //创建"第几节数"视图
-    private void createLeftView(Course course) {
-        int endNumber = course.getEnd();
-        if (endNumber > maxCoursesNumber) {
-            for (int i = 0; i < endNumber-maxCoursesNumber; i++) {
-                View view = LayoutInflater.from(this).inflate(R.layout.left_view, null);
-                LinearLayout.LayoutParams params = new LinearLayout.LayoutParams(110,180);
-                view.setLayoutParams(params);
-
-                TextView text = view.findViewById(R.id.class_number_text);
-                text.setText(String.valueOf(++currentCoursesNumber));
-
-                LinearLayout leftViewLayout = findViewById(R.id.left_view_layout);
-                leftViewLayout.addView(view);
-            }
-            maxCoursesNumber = endNumber;
-        }
-    }
+//    //从数据库加载数据
+//    private void loadData(){
+//        ArrayList<Course> coursesList = new ArrayList<>();//课程列表
+//        SQLiteDatabase sqLiteDatabase = databaseHelper.getWritableDatabase();
+//        Cursor cursor=sqLiteDatabase.rawQuery("select * from courses",null);
+//        if(cursor.moveToFirst()){
+//            do {
+//                coursesList.add(new Course(
+//                        cursor.getString(cursor.getColumnIndex("course_name")),
+//                        cursor.getString(cursor.getColumnIndex("teacher")),
+//                        cursor.getString(cursor.getColumnIndex("class_room")),
+//                        cursor.getInt(cursor.getColumnIndex("day")),
+//                        cursor.getInt(cursor.getColumnIndex("class_start")),
+//                        cursor.getInt(cursor.getColumnIndex("class_end"))));
+//            }while (cursor.moveToNext());
+//        }
+//        cursor.close();
+//
+//        //使用从数据库读取出来的课程信息来加载课程表视图
+//        for (Course course : coursesList) {
+//            createLeftView(course);
+//            createItemCourseView(course);
+//        }
+//    }
+//
+//    //保存数据到数据库
+//    private void saveData(Course course) {
+//        SQLiteDatabase sqLiteDatabase =  databaseHelper.getWritableDatabase();
+//        sqLiteDatabase.execSQL
+//                ("insert into courses(course_name, teacher, class_room, day, class_start, class_end) " + "values(?, ?, ?, ?, ?, ?)",
+//                        new String[] {course.getCourseName(),
+//                                course.getTeacher(),
+//                                course.getClassRoom(),
+//                                course.getDay()+"",
+//                                course.getStart()+"",
+//                                course.getEnd()+""}
+//                );
+//    }
+//
+//    //创建"第几节数"视图
+//    private void createLeftView(Course course) {
+//        int endNumber = course.getEnd();
+//        if (endNumber > maxCoursesNumber) {
+//            for (int i = 0; i < endNumber-maxCoursesNumber; i++) {
+//                View view = LayoutInflater.from(this).inflate(R.layout.left_view, null);
+//                LinearLayout.LayoutParams params = new LinearLayout.LayoutParams(110,180);
+//                view.setLayoutParams(params);
+//
+//                TextView text = view.findViewById(R.id.class_number_text);
+//                text.setText(String.valueOf(++currentCoursesNumber));
+//
+//                LinearLayout leftViewLayout = findViewById(R.id.left_view_layout);
+//                leftViewLayout.addView(view);
+//            }
+//            maxCoursesNumber = endNumber;
+//        }
+//    }
     private void init() {
         typeface = Typeface.createFromAsset(getAssets(),"iconfont/iconfont.ttf");
         Button back = findViewById(R.id.back);
         back.setTypeface(typeface);
         //调用图标
-        TextView add=findViewById(R.id.add);
-        add.setTypeface(typeface);
+        TextView ba=findViewById(R.id.back);
+        back.setTypeface(typeface);
         back.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 finish();
             }
         });
+//        typeface = Typeface.createFromAsset(getAssets(),"iconfont/iconfontadd.ttf");
+//        Button add = findViewById(R.id.addCourse);
+//        add.setTypeface(typeface);
+//        //调用图标
+//        TextView addc=findViewById(R.id.add);
+//        add.setTypeface(typeface);
+//        back.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                startActivity(new Intent(TimetableActivity.this,AddcourseActivity.class));
+//            }
+//        });
     }
 }
