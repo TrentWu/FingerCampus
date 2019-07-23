cd2645d47c62b352c123524c94e560571f4c74ee
传成 吴 <429223306@qq.com>
2019-07-23 18:16:21 +0800

    修复“添加课程表”功能 添加签到界面扫面二维码功能

--- app/src/main/java/com/example/fingercampus/Timetable/AddCourseActivity.java
+++ app/src/main/java/com/example/fingercampus/Timetable/AddCourseActivity.java
@@ -28,16 +28,13 @@ import java.util.Map;
 public class AddCourseActivity extends Activity {
 
     private Spinner select_csday;
+    private Spinner select_csbtime;
+    private Spinner select_csetime;
     private Spinner select_csbweek;
     private Spinner select_cseweek;
 
     private Button finishButton;
     private EditText csname_edit;
-    private EditText csbweek_edit;
-    private EditText cseweek_edit;
-    private Spinner csday_edit;
-    private Spinner csbtime_edit;
-    private Spinner csetime_edit;
     private EditText classroom_edit;
     private EditText teacher_edit;
     private String TAG = "AddCourseActivity";
@@ -46,66 +43,74 @@ public class AddCourseActivity extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.addcourse);
-//        setFinishOnTouchOutside(false);
+        setContentView(R.layout.timetable_addcourse);
 
-        select_csday = (Spinner)findViewById(R.id.csday);
-        select_csbweek = (Spinner)findViewById(R.id.csbtime);
-        select_cseweek = (Spinner)findViewById(R.id.csetime);
+        select_csday = findViewById(R.id.csday);
+        select_csbtime = findViewById(R.id.csbtime);
+        select_csetime = findViewById(R.id.csetime);
+        select_csbweek = findViewById(R.id.csbweek);
+        select_cseweek = findViewById(R.id.cseweek);
 
-        csname_edit = (EditText) findViewById(R.id.csname);
-        csbweek_edit = (EditText)findViewById(R.id.csbweek);
-        cseweek_edit = (EditText)findViewById(R.id.cseweek);
-        csday_edit = (Spinner)findViewById(R.id.csday);
-        csbtime_edit = (Spinner)findViewById(R.id.csbtime);
-        csetime_edit = (Spinner)findViewById(R.id.csetime);
-        classroom_edit = (EditText)findViewById(R.id.clrid);
-        teacher_edit= (EditText)findViewById(R.id.tcid);
+        csname_edit = findViewById(R.id.csname);
+        classroom_edit = findViewById(R.id.clrid);
+        teacher_edit = findViewById(R.id.tcid);
 
-        finishButton = (Button) findViewById(R.id.finish);
+        finishButton = findViewById(R.id.finish);
         finishButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 String csname = csname_edit.getText().toString().trim();
-                String csbweek = csbweek_edit.getText().toString().trim();
-                String cseweek = cseweek_edit.getText().toString().trim();
+                String csbweek = select_csbweek.getSelectedItem().toString();
+                String cseweek = select_cseweek.getSelectedItem().toString();
                 String csday = select_csday.getSelectedItem().toString();
-                String csbtime = select_csbweek.getSelectedItem().toString();
-                if(csbtime=="一")
-                {
-                    csbtime="08:00:00";
-                }else if(csbtime=="二"){
-                    csbtime="10:00:00";
-                }else if(csbtime=="三"){
-                    csbtime="14:00:00";
-                }else if(csbtime=="四"){
-                    csbtime="16:00:00";
-                }else if(csbtime=="五"){
-                    csbtime="19:00:00";
-                }else if(csbtime=="六"){
-                    csbtime="21:00:00";
+                String csbtime = select_csbtime.getSelectedItem().toString();
+                switch (csbtime) {
+                    case "一":
+                        csbtime = "08:00:00";
+                        break;
+                    case "二":
+                        csbtime = "10:10:00";
+                        break;
+                    case "三":
+                        csbtime = "14:00:00";
+                        break;
+                    case "四":
+                        csbtime = "16:00:00";
+                        break;
+                    case "五":
+                        csbtime = "19:00:00";
+                        break;
+                    case "六":
+                        csbtime = "21:00:00";
+                        break;
                 }
-                String csetime = select_cseweek.getSelectedItem().toString();
-                if(csetime=="一")
-                {
-                    csetime="08:00:00";
-                }else if(csbtime=="二"){
-                    csetime="10:00:00";
-                }else if(csbtime=="三"){
-                    csetime="14:00:00";
-                }else if(csbtime=="四"){
-                    csetime="16:00:00";
-                }else if(csbtime=="五"){
-                    csetime="19:00:00";
-                }else if(csbtime=="六"){
-                    csetime="21:00:00";
+                String csetime = select_csetime.getSelectedItem().toString();
+                switch (csetime) {
+                    case "一":
+                        csetime = "09:50:00";
+                        break;
+                    case "二":
+                        csetime = "12:00:00";
+                        break;
+                    case "三":
+                        csetime = "15:50:00";
+                        break;
+                    case "四":
+                        csetime = "17:50:00";
+                        break;
+                    case "五":
+                        csetime = "20:50:00";
+                        break;
+                    case "六":
+                        csetime = "21:50:00";
+                        break;
                 }
                 String classroom = classroom_edit.getText().toString().trim();
                 String teacher = teacher_edit.getText().toString().trim();
-                if (csname==null||csname.isEmpty() || csbweek==null||csbweek.isEmpty() || cseweek==null||cseweek.isEmpty() || csday==null||csday.isEmpty() || csbtime==null||csbtime.isEmpty() || csetime==null||csetime.isEmpty()){
+                if (csname.isEmpty() || csbweek.isEmpty() || cseweek.isEmpty() || csday.isEmpty() || csbtime.isEmpty() || csetime.isEmpty()) {
                     Toast.makeText(AddCourseActivity.this, "请输入完整信息！", Toast.LENGTH_SHORT).show();
-                }else{
-                    TimetableRequest(csname,csbweek,cseweek,csday,csbtime,csetime,classroom,teacher);
+                } else {
+                    TimetableRequest(csname, csbweek, cseweek, csday, csbtime, csetime, classroom, teacher);
                 }
             }
         });
@@ -116,15 +121,15 @@ public class AddCourseActivity extends Activity {
      * 用户登录请求类
      *
      * @param csname    课程名
-     * @param cwbtime   开始周次
-     * @param cwetime   结束周次
-     * @param csday       星期几
+     * @param csbweek   开始周次
+     * @param cseweek   结束周次
+     * @param csday     星期几
      * @param csbtime   开始节次
      * @param csetime   结束节次
-     * @param clrid     教室
-     * @param tcid      教师
+     * @param clrname   教室名称
+     * @param tcname    教师名称
      */
-    public void TimetableRequest(final String csname, final String csbweek, final String cseweek, final String csday, final String csbtime, final String csetime, final String clrid, final String tcid) {
+    public void TimetableRequest(final String csname, final String csbweek, final String cseweek, final String csday, final String csbtime, final String csetime, final String clrname, final String tcname) {
         //请求地址
         String url = "http://119.3.232.205:8080/FingerCampus/*";    //注①
         final String tag = "Timetable";    //注②
@@ -144,11 +149,10 @@ public class AddCourseActivity extends Activity {
                     JSONObject jsonObject = (JSONObject) new JSONObject(response).get("params");  //注③
                     String result = jsonObject.getString("Result");  //注④
                     if (result.equals("success")) {  //注⑤
-                        Toast.makeText(AddCourseActivity.this, "导入成功！" , Toast.LENGTH_LONG).show();
+                        Toast.makeText(AddCourseActivity.this, "导入成功！", Toast.LENGTH_LONG).show();
                         startActivity(new Intent(AddCourseActivity.this, TimetableActivity.class));
                         finish();
-                    }
-                    else{
+                    } else {
                         Toast.makeText(AddCourseActivity.this, "导入失败！", Toast.LENGTH_SHORT).show();
                     }
                 } catch (JSONException e) {
@@ -175,16 +179,8 @@ public class AddCourseActivity extends Activity {
                 params.put("csday", csday);
                 params.put("csbtime", csbtime);
                 params.put("csetime", csetime);
-                params.put("clrid", clrid);
-                params.put("tcid", tcid);
-                LogUtil.d(TAG, "csname=" + csname);
-                LogUtil.d(TAG, "csbweek=" + csbweek);
-                LogUtil.d(TAG, "cseweek=" + cseweek);
-                LogUtil.d(TAG, "csday=" + csday);
-                LogUtil.d(TAG, "csbtime=" + csbtime);
-                LogUtil.d(TAG, "csetime=" + csetime);
-                LogUtil.d(TAG, "clrid=" + clrid);
-                LogUtil.d(TAG, "tcid=" + tcid);
+                params.put("clrname", clrname);
+                params.put("tcname", tcname);
                 return params;
             }
         };
